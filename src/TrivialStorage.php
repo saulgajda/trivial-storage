7042c2e2b3ff5af10cb51e4cfa19adc5839c9fe7
Paweł Gajda <saulgajda@gmail.com>
2019-05-28 00:07:11 +0200

    Initial commit

---
+++ src/TrivialStorage.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace SaulGajda\TrivialStorage;
+
+class TrivialStorage {
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+        $this->load();
+    }
+
+    public function create($key, $item) {
+        $this->data[$key] = $item;
+    } 
+
+    public function read($key) {
+        if ( !array_key_exists($key, $this->data ) ) {
+            return NULL;
+        }
+
+        return $this->data[$key];
+    }
+
+    public function readAll() {
+        return $this->data;
+    }
+
+    public function update($key, $item) {
+        $this->data[$key] = $item;
+    }
+
+    public function delete($key) {
+        if ( !array_key_exists($key, $this->data ) ) {
+            return;
+        }
+
+        unset( $this->data[$key] );
+        $this->save();
+    }
+
+    public function deleteAll() {
+        $this->data = [];
+        $this->save();
+    }
+
+    private $filename = 'data.json';
+    private $data = [];
+
+    private function load() {
+        $this->data = json_decode(file_get_contents($this->filename));
+    }
+
+    private function save() {
+        file_put_contents($this->filename, json_encode($this->data));
+    }
+}
